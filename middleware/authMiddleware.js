34c1770518323c3eac25560304d119b87f19777d
priyanshumaurya700 <priyanshupratapm@gmail.com>
2026-02-11 17:24:00 +0530

    new changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,27 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const protect = (req, res, next)=>{
+export const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        if(!token){
-            res.status(401).json({message: 'Not authorized, no token'})
-        }
+      token = req.headers.authorization.split(" ")[1];
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.id;
-        next();
-    }catch (error) {
-        res.status(401).json({message: 'Not authorized, token failed'})
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
-}
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
