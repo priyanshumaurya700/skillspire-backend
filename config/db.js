1328e302bfde90b074b5d400475a398419e2e4ba
priyanshumaurya700 <priyanshupratapm@gmail.com>
2026-02-12 22:51:29 +0530

    admin created

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
+        console.log("MongoDB connected successfully", mongoose.connection.name);
     }catch(error){
         console.error("MongoDB connection failed:", error);
         process.exit(1);
