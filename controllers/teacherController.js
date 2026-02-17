9de4f92581f47d47b992a491a3a9a63a1466450b
priyanshumaurya700 <priyanshupratapm@gmail.com>
2026-02-17 11:22:21 +0530

    course assign

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,6 +1,7 @@
+import CourseAssignment from "../models/CourseAssignment.js";
 import registereduser from "../models/User.js";
 
-
+// Get all teachers
 export const getTeachers = async (req, res) => {
   try {
     const teachers = await registereduser.find({ role: "teacher" });
@@ -11,5 +12,29 @@ export const getTeachers = async (req, res) => {
       message: "Failed to fetch teachers",
       error: error.message,
     });
+  }
+};
+
+//  get assigned courses for teacher
+
+export const getAssignedCourses = async (req, res) => {
+  try {
+    const teacherId = req.user.id;
+    const assignments = await CourseAssignment.find({teacherId}).populate("courseId", "title description");
+    
+    const course = assignments.map(assignment => ({
+      courseId: assignment.courseId.id,
+      title: assignment.courseId.title,
+      description: assignment.courseId.description,
+    }));
+    res.status(200).json({ assignments, course });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to fetch assigned courses",
+        error: error.message,
+      });
   }
 };
--- routes/teacherRouter.js
+++ routes/teacherRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getTeachers } from "../controllers/teacherController.js";
+import { getAssignedCourses, getTeachers } from "../controllers/teacherController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const teacherRouter = express.Router();
 
 // get teacher
 teacherRouter.get("/teachers", getTeachers);
+
+// get assigned courses for teacher
+teacherRouter.get("/assigned-courses", protect, getAssignedCourses);
 
 
 export default teacherRouter;
